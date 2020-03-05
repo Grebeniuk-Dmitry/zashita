8b53dde9ce078e38f807040c13283d92bdfcb7bd
hammerzem <hammerzem@gmail.com>
2020-03-05 21:36:18 +0200

    modal

--- js/main.js
+++ js/main.js
@@ -1,6 +1,51 @@
-
-function btn() {
+/*function btn() {
 	let z = document.getElementsByClassName("zem")[0];
 	console.log(z);
 	z.setAttribute('style','display: flex;');
 	}
+	*/
+/*var arr = document.getElementsByTagName("div");
+for (var x=0; x<arr.length; x++)
+{
+	arr[x].innerHTML="Solo";
+}
+*/
+
+
+/*let arr = document.getElementsByClassName('advanse__column__col')[0];
+arr.setAttribute('style', 'color: blue;')
+console.log(document.getElementsByTagName('img'))*/
+/*for (var i = 4; i <=7; i++) {
+    (function(i) {
+        setTimeout(function() { let img = document.getElementsByTagName('img') [i];
+img.setAttribute('style', 'background-color: #fff')}, 300 * i);
+    })(i);
+}*/
+
+ 
+
+
+var modal = document.getElementById('myModal');
+
+
+var btn = document.getElementById("kont");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+    modal.style.display = "flex";
+}
+
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
